682579cb9eae797f3ddf0b7fae612fe088c2fb67
char-lie <5962274+char-lie@users.noreply.github.com>
2016-02-24 20:37:08 +0200

    Using faker to generate new data

--- __main__.py
+++ __main__.py
@@ -1,26 +1,38 @@
 import psycopg2
 from random import randint
 from os import linesep
+from faker import Faker
+from datetime import datetime, timedelta
 
 from src import TransactionManager
 
 
-def get_booking_connection():
-    connection_string = "host='localhost' dbname='db_booking' user='postgres' password='12345'"
+USER = 'test_user'
+PASSWORD = '123'
+HOST = 'localhost'
+CONNECTION_PREFIX = "host='{host}' user='{user}' password='{password}'".format(
+                     host=HOST, user=USER, password=PASSWORD)
+
+
+def get_fly_connection():
+    connection_string = "{connection} dbname='{db}'".format(
+                          connection=CONNECTION_PREFIX, db='test_db_fly')
     return psycopg2.connect(connection_string)
 
 
 def get_hotel_connection():
-    connection_string = "host='localhost' dbname='db_hotel' user='postgres' password='12345'"
+    connection_string = "{connection} dbname='{db}'".format(
+                          connection=CONNECTION_PREFIX, db='test_db_hotel')
     return psycopg2.connect(connection_string)
 
 
-def book(tm, values):
+def fly(tm, values):
 
-    query_string = "insert into test values (%d);"
-    queries = [query_string%v for v in values]
+    query_string = "insert into fly_booking (client_name, fly_number, fly_from, fly_to, book_date) values ('{client}', '{number}', '{fly_from}', '{fly_to}', '{book_date}');"
+    queries = [query_string.format(**value) for value in values]
+    #print linesep.join(queries)
 
-    connection = get_booking_connection()
+    connection = get_fly_connection()
     cursor = connection.cursor()
 
     success = tm.add_transaction(cursor, queries)
@@ -28,7 +40,7 @@ def book(tm, values):
     if not success:
         return connection
 
-    print 'Commands booking ready'.upper()
+    print 'Commands fly ready'.upper()
     print linesep.join(queries)
 
     return connection
@@ -36,8 +48,9 @@ def book(tm, values):
 
 def hotel(tm, values):
 
-    query_string = "insert into test_hotel values ('%s');"
-    queries = [query_string%v for v in values]
+    query_string = "insert into hotel_booking (client_name, hotel_name, arrival_date, departure_date) values ('{client}', '{hotel}', '{arrival}', '{departure}');"
+    queries = [query_string.format(**value) for value in values]
+    #print linesep.join(queries)
 
     connection = get_hotel_connection()
     cursor = connection.cursor()
@@ -58,10 +71,26 @@ if __name__ == '__main__':
 
     tm = TransactionManager()
 
-    booking_connection = book(tm, [randint(0, 1000) for i in range(2)])
-    hotel_connection = hotel(tm, ['a', 'b'])
+    faker = Faker()
+
+    fly_book = {
+        'client': faker.name().replace('\'', '\\\''),
+        'number': faker.sha1(),
+        'fly_from': faker.country_code(),
+        'fly_to': faker.country_code(),
+        'book_date': str(faker.date_time_this_month())
+    }
+    hotel_book = {
+        'client': faker.name().replace('\'', '\\\''),
+        'hotel': faker.address().replace('\'', '\\\'').replace('\n', '; '),
+        'arrival': str(faker.date_time_this_month()),
+        'departure': str(datetime.now() + timedelta(days=faker.random_digit()))
+    }
+    fly_connection = fly(tm, [fly_book])
+    hotel_connection = hotel(tm, [hotel_book])
 
     tm.commit_transactions()
 
-    booking_connection.close()
+    fly_connection.close()
+    hotel_connection.close()
 
