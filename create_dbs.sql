d29275162d07a9419658fb3dc11df0fefd27e5af
char-lie <5962274+char-lie@users.noreply.github.com>
2016-02-24 16:42:40 +0200

    Created queries for test database and user creation and deletion

---
+++ create_dbs.sql
@@ -0,0 +1,4 @@
+create user test_user with password '123';
+create database test_db_hotel owner test_user;
+create database test_db_booking owner test_user;
+
---
+++ drop_dbs.sql
@@ -0,0 +1,4 @@
+drop database test_db_hotel;
+drop database test_db_booking;
+drop user test_user;
+
